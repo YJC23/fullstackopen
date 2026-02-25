e7cfd8fd14a9f1ec4d3e459ae7647ff31e46a2fb
YoungJu Choi <yjchoiofficial@gmail.com>
2026-02-25 11:03:27 +0000

    Finished part 3a

---
+++ part3/phonebook/index.js
@@ -0,0 +1,122 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+// 1. Define your custom token
+morgan.token('body', (req) => JSON.stringify(req.body));
+
+// 2. Use a function to define the format dynamically
+app.use(morgan((tokens, req, res) => {
+  // Start with the standard 'tiny' components
+  const log = [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms'
+  ];
+
+  // 3. Only append the body if the method is POST
+  if (req.method === 'POST') {
+    log.push(tokens.body(req, res));
+  }
+
+  return log.join(' ');
+}));
+
+app.use(express.json())
+
+let  persons = [
+  { 
+    "id": "1",
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": "2",
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": "3",
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": "4",
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+  
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.get('/info', (request, response) => {
+  const currentTime = new Date()
+
+  response.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>Time: ${currentTime} </p>
+  `)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  return Math.floor(Math.random() * 10000)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+   
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name is missing' 
+    })
+  }
+  else if (!body.number) {
+    return response.status(400).json({
+      error: 'number is missing'
+    })
+  }
+  else if (persons.find(person => person.name === body.name)) {
+    return response.status(409).json({
+      error: 'name must be unique'
+    })
+  }
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+  response.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
