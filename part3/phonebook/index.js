be2d871203c89360c0b6b48bea9327073fa17819
YoungJu Choi <yjchoiofficial@gmail.com>
2026-02-27 09:15:29 +0000

    ready for webservice

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,6 +2,29 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
+let  persons = [
+  { 
+    "id": "1",
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": "2",
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": "3",
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": "4",
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
 // 1. Define your custom token
 morgan.token('body', (req) => JSON.stringify(req.body));
 
@@ -25,29 +48,6 @@ app.use(morgan((tokens, req, res) => {
 }));
 
 app.use(express.json())
-
-let  persons = [
-  { 
-    "id": "1",
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": "2",
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": "3",
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": "4",
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
--- part3/test/index.js
+++ part3/test/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 let notes = [
   {
@@ -27,8 +28,8 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+app.use(cors())
 app.use(express.json())
-// app.use(requestLogger())
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -88,7 +89,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
